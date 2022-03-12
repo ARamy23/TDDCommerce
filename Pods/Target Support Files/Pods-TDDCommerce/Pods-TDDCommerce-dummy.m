61f0d770e2d7c9b9d9aee55938210f1921cfedd0
Ahmed Ramy <dev.ahmedramy@icloud.com>
2022-03-12 06:25:30 +0200

    Remove Pods from Git tracking

--- Pods/Target Support Files/Pods-TDDCommerce/Pods-TDDCommerce-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TDDCommerce : NSObject
-@end
-@implementation PodsDummy_Pods_TDDCommerce
-@end
--- Pods/Target Support Files/Pods-TDDCommerceTests/Pods-TDDCommerceTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TDDCommerceTests : NSObject
-@end
-@implementation PodsDummy_Pods_TDDCommerceTests
-@end
