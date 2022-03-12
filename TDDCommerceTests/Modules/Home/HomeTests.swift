f89cd2d9d717801cd27ffa0f70fcf16f60414354
Ahmed Ramy <dev.ahmedramy@icloud.com>
2022-03-12 06:41:45 +0200

    Write better contexts

--- TDDCommerceTests/Modules/Home/HomeTests.swift
+++ TDDCommerceTests/Modules/Home/HomeTests.swift
@@ -14,13 +14,15 @@ class HomeTests: QuickSpec {
     override func spec() {
         describe("home page") {
             let sut = HomeViewModel()
-            context("previewing") {
-                context("when items are present") {
-                    let expectedModels = [Item].stubbed()
-                    network.expectedModel = expectedModels
-                    it("displays the items to the user") {
-                        sut.fetchItems()
-                        expect(sut.state).to(be(.loaded(expectedModels)))
+            context("on first launch") {
+                context("and fetching items") {
+                    context("without failing") {
+                        let expectedModels = [Item].stubbed()
+                        network.expectedModel = expectedModels
+                        it("displays the items to the user") {
+                            sut.fetchItems()
+                            expect(sut.state).to(be(.loaded(expectedModels)))
+                        }
                     }
                 }
             }
