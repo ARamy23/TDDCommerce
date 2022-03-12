bfddd0c0058ffbbb1ed2bc887e4f43dafc4988cd
Ahmed Ramy <dev.ahmedramy@gmail.com>
2022-03-12 06:38:33 +0200

    Write failing test for when Home is first loaded

--- TDDCommerceTests/Modules/Home/HomeTests.swift
+++ TDDCommerceTests/Modules/Home/HomeTests.swift
@@ -13,10 +13,14 @@ import Nimble
 class HomeTests: QuickSpec {
     override func spec() {
         describe("home page") {
+            let sut = HomeViewModel()
             context("previewing") {
                 context("when items are present") {
+                    let expectedModels = [Item].stubbed()
+                    network.expectedModel = expectedModels
                     it("displays the items to the user") {
-                        
+                        sut.fetchItems()
+                        expect(sut.state).to(be(.loaded(expectedModels)))
                     }
                 }
             }
