266b373f08be092ef0966aeb0de6d2d01b447b79
Ahmed Ramy <dev.ahmedramy@icloud.com>
2022-03-12 06:31:46 +0200

    Adding Home first test

---
+++ TDDCommerceTests/Modules/Home/HomeTests.swift
@@ -0,0 +1,25 @@
+//
+//  HomeTests.swift
+//  TDDCommerceTests
+//
+//  Created by Ahmed Ramy on 12/03/2022.
+//
+
+import XCTest
+import Quick
+import Nimble
+@testable import TDDCommerce
+
+class HomeTests: QuickSpec {
+    override func spec() {
+        describe("home page") {
+            context("previewing") {
+                context("when items are present") {
+                    it("displays the items to the user") {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
